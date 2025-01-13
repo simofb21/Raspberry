7c0f5d4a70d36d90874ff05f4edc39cdfc6963ef
simofb21 <simone.fusarbassini@galileo.galileicrema.it>
2025-01-13 09:30:47 +0100

    Aggiunta run se no non funzionava, aggiunto anche un link  al sito mio

--- provaPython.py
+++ provaPython.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p> <a href='https://fb-bike.it'>Clicca qui</a>"
+
+if __name__ == "__main__":
+    app.run()
